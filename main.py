d27997c0538dfcafa71b30acefbea72c5e3c82a0
Developer <dev@screenshare.com>
2026-07-25 23:48:57 +0530

    added all http methods

--- main.py
+++ main.py
@@ -2,6 +2,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Get method
 @app.get("/")
 def home():
     return {"message": "Hello, welcome to learning FastAPI!"}
+
+@app.get("/students")
+def students():
+    return ["Ashwini","Ash","Rash"]
+
+# POST method
+@app.post("/post")
+def add_students():
+    return {"message": "added student"}
+
+#Update the dat
+@app.put("/students")
+def update_student():
+    return {"message": "Student Updated"}
+
+# Delete the data
+@app.delete("/students")
+def delete_student():
+    return {"message": "Student Deleted"}
